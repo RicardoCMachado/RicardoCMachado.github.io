0420f5f5768d77ab8f7f96af4ff0d5e6113c1c5f
Ricardo Machado <ricardo.machado@ua.pt>
2023-05-21 19:48:21 +0100

    camera movement

---
+++ bib/helper.js
@@ -0,0 +1,5 @@
+var playButton = document.getElementById("play_button");
+        playButton.addEventListener("click", function() {
+            var menu = document.getElementById("menu");
+            menu.classList.add("hide");
+        });
